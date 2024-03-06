da18888dd6bae7741163183511856182fcb830d6
mThkTrn <madhavendra.thakur26@trinityschoolnyc.org>
2024-03-06 11:53:17 -0500

    added code

--- server.py
+++ server.py
@@ -2,13 +2,42 @@
 from flask import Flask
 from flask import render_template
 import json
-
+import os
 
 app = Flask(__name__, static_url_path='', static_folder='static')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    filename = os.path.join(app.root_path, 'data', 'life_expectancy.json')
+
+    with open(filename) as test_file:
+        data = json.load(test_file)
+    polylines = []
+    labels_y_vals = []
+    for country in ["Canada", "Mexico", "United States"]:
+
+        polyline = []
+
+        for year in range(1960, 2021):
+            out = []
+            out.append(str(50 + (year-1960)*6.66))
+            out.append(str(450-4*(data[country][str(year)])))
+            print(out)
+            polyline.append(out)
+            if year == 2020:
+                labels_y_vals.append(out[1])
+        
+        for i in range(len(polyline)):
+            polyline[i] = ",".join(polyline[i])
+        polyline = " ".join(polyline)
+        print("--")
+        print(polyline)
+        print("--")
+        polylines.append(polyline)
+
+
+
+    return render_template('index.html', polylines = polylines, labels_y_vals = labels_y_vals, data = data)
 
 @app.route('/year')
 def year():
