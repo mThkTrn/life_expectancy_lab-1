2c18617f2272cf71b595b6993587a0644afdca46
mThkTrn <madhavendra.thakur26@trinityschoolnyc.org>
2024-03-07 12:59:23 -0500

    added more code

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 # flask --app data_server run
 from flask import Flask
 from flask import render_template
+from flask import request
 import json
 import os
 
@@ -27,7 +28,7 @@ def index():
             out.append(str(50 + (year-1960)*6.66))
             out.append(str(450-4*(data[country][str(year)])))
 
-            avg_sum += data[country][str(year)]
+            avg_sum += float(out[1])
             avg_count += 1
             print(out)
             polyline.append(out)
@@ -42,14 +43,32 @@ def index():
         print("--")
         polylines.append(polyline)
 
-        avg = avg_sum/avg_count
+        avg_height = avg_sum/avg_count
+        print(f"avg_height = {avg_height}")
 
 
 
-    return render_template('index.html', polylines = polylines, labels_y_vals = labels_y_vals, data = data, avg = avg)
+    return render_template('index.html', polylines = polylines, labels_y_vals = labels_y_vals, data = data, avg_height = avg_height)
 
 @app.route('/year')
 def year():
-    return render_template('year.html')
+
+    filename = os.path.join(app.root_path, 'data', 'life_expectancy.json')
+
+    with open(filename) as test_file:
+        data = json.load(test_file)
+
+    requested_year = request.args.get("year")
+
+    expectancies = [data["Canada"][requested_year], data["Mexico"][requested_year], data["United States"][requested_year]]
+
+    def expectancyToColor(expectancy):
+        return f"({round(255-(expectancy * (255/100)))},{round(255-(expectancy * (255/100)))}, 255)"
+    
+    colors = [expectancyToColor(j) for j in expectancies]
+
+    print(colors)
+
+    return render_template('year.html', year = requested_year, colors = colors)
 
 app.run(debug=True)
